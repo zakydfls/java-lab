56dcdf0669fb5b5a3863eed39539ff168d36fe7d
Zaky Dafalas Eka Putra <zakydaffeka26@gmail.com>
2023-04-07 14:04:43 +0700

    add fitur jml ips

--- pertemuan-5/arraylist/MahasiswaAksi.java
+++ pertemuan-5/arraylist/MahasiswaAksi.java
@@ -13,12 +13,15 @@ public class MahasiswaAksi {
 
         // membuat input nama, nim, dan nilai
         while (menu == 'Y' || menu == 'y') {
-            System.out.print("Masukkan nama mahasiswa: ");
+            System.out.print("Masukkan nama mahasiswa                   : ");
             String nama = inp.nextLine();
-            System.out.print("Masukkan NIM mahasiswa: ");
+            System.out.print("Masukkan NIM mahasiswa                    : ");
             String nim = inp.nextLine();
             ArrayList<Double> nilai = new ArrayList<Double>();
-            for (int i = 1; i <= 8; i++) {
+            System.out.print("Masukkan jumlah IPS yang ingin di Input   : ");
+            int jml = inp.nextInt();
+            inp.nextLine();
+            for (int i = 1; i <= jml; i++) {
                 System.out.print("Masukkan IP semester " + (0 + i) + " : ");
                 String inputNilai = inp.nextLine();
                 nilai.add(Double.parseDouble(inputNilai));
@@ -36,7 +39,7 @@ public class MahasiswaAksi {
                 Mahasiswa mhs = iterator.next();
                 System.out.println("Nama: " + mhs.getNama() + ", NIM: " + mhs.getNim() + ", IPK: " + mhs.hitungIPK());
             }
-            System.out.print("Input data lagi ? [Y/T] ");
+            System.out.print("Input data mahasiswa baru ? [Y/T] ");
             menu = inp.next().charAt(0);
             inp.nextLine();
         }
